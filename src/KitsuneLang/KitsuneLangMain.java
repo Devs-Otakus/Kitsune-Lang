4b45018ff6805f3928044c2707601bea8c053a4a
Luiz Guilherme <37917447+Luiz-Batata-Guilherme@users.noreply.github.com>
2025-05-27 20:07:12 -0300

    Add main class and initial welcome message for KitsuneLang

---
+++ src/KitsuneLang/KitsuneLangMain.java
@@ -0,0 +1,7 @@
+public class KitsuneLangMain {
+    public static void main(String[] args) {
+        System.out.println("Welcome to KitsuneLang!");
+        // Here you can add more functionality or call other methods/classes
+        // to start the KitsuneLang interpreter or compiler.
+    }
+}
