d710783010f7079ab0fd8b2d6f3508e528662b1e
Luiz Guilherme <37917447+Luiz-Batata-Guilherme@users.noreply.github.com>
2025-05-29 20:42:59 -0300

    Refactor main method and add error handling for KitsuneLang execution

--- src/KitsuneLang/KitsuneLangMain.java
+++ src/KitsuneLang/KitsuneLangMain.java
@@ -1,11 +1,34 @@
 package KitsuneLang;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.nio.charset.Charset;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.List;
+
+import KitsuneLang.lexer.ScannerKitsuneLang;
+import KitsuneLang.lexer.Token;
+
 /**
  * KitsuneLangMain.java This is the main entry point for the KitsuneLang
  * programming language. It initializes the interpreter or compiler and starts
  * the execution.
  */
 public class KitsuneLangMain {
+    static boolean hadError = false;
+
+    private static void report(int line, String where, String message) {
+        System.err
+                .println("[line " + line + "] Error" + where + ": " + message);
+        hadError = true;
+    }
+
+    public static void error(int line, String message) {
+        report(line, "", message);
+    }
+
     /**
      * The main method serves as the entry point for the KitsuneLang
      * application. It can be used to initialize the interpreter or compiler and
@@ -13,9 +36,38 @@ public class KitsuneLangMain {
      *
      * @param args Command line arguments (not used in this example).
      */
-    public static void main(String[] args) {
-        System.out.println("Welcome to KitsuneLang!");
-        // Here you can add more functionality or call other methods/classes
-        // to start the KitsuneLang interpreter or compiler.
+    public static void main(String[] args) throws IOException {
+        runPrompt();
+    }
+
+    private static void runFile(String path) throws IOException {
+        byte[] bytes = Files.readAllBytes(Paths.get(path));
+        run(new String(bytes, Charset.defaultCharset()));
+        if (hadError)
+            System.exit(65);
+    }
+
+    private static void runPrompt() throws IOException {
+        InputStreamReader input = new InputStreamReader(System.in);
+        BufferedReader reader = new BufferedReader(input);
+
+        for (;;) {
+            System.out.print("> ");
+            String line = reader.readLine();
+            if (line == null)
+                break;
+            run(line);
+            hadError = false;
+        }
+    }
+
+    private static void run(String source) {
+
+        ScannerKitsuneLang scan = new ScannerKitsuneLang(source);
+        List<Token> tokens = scan.scanTokens();
+
+        for (Token token : tokens) {
+            System.out.println(token);
+        }
     }
 }
