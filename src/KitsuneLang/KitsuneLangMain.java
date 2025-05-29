08b65138702399f9d1ea1cbf556efba44a4325d7
Luiz Guilherme <37917447+Luiz-Batata-Guilherme@users.noreply.github.com>
2025-05-29 20:48:16 -0300

    Refactor main method to handle command line arguments and improve usage instructions

--- src/KitsuneLang/KitsuneLangMain.java
+++ src/KitsuneLang/KitsuneLangMain.java
@@ -37,7 +37,14 @@ public class KitsuneLangMain {
      * @param args Command line arguments (not used in this example).
      */
     public static void main(String[] args) throws IOException {
-        runPrompt();
+        if (args.length > 1) {
+            System.out.println("Usage: jlox [script]");
+            System.exit(64);
+        } else if (args.length == 1) {
+            runFile(args[0]);
+        } else {
+            runPrompt();
+        }
     }
 
     private static void runFile(String path) throws IOException {
